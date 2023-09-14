b7ca7c396556dccab7db54d7c571e61aa9c89cc7
mattpsvreis <mateuspsvreis@gmail.com>
2023-09-14 14:09:16 -0300

    feat: :sparkles: Impleting the usage of OpenAI to generate audio to text transcriptions and then generating a summary of the text

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,20 @@
 import { fastify } from 'fastify';
+import { fastifyCors } from '@fastify/cors';
 import { getAllPromptsRoute } from './routes/getAllPrompts';
 import { uploadVideoRoute } from './routes/uploadVideo';
+import { createTranscriptionRoute } from './routes/createTranscription';
+import { generateAICompletionRoute } from './routes/generateAICompletion';
 
 const app = fastify();
 
+app.register(fastifyCors, {
+  origin: '*',
+});
+
 app.register(getAllPromptsRoute);
 app.register(uploadVideoRoute);
+app.register(createTranscriptionRoute);
+app.register(generateAICompletionRoute);
 
 app
   .listen({
